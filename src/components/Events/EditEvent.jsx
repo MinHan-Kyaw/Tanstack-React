547de0c40925c4c05703bf4d6872ab500d4e77f2
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-09 12:27:44 +0700

    Optimistic Updating

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -6,6 +6,7 @@ import EventForm from "./EventForm.jsx";
 import { fetchEvent, updateEvent } from "../../utils/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { queryClient } from "../../utils/http.js";
 
 export default function EditEvent() {
   const navigate = useNavigate();
@@ -18,11 +19,26 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+
+      queryClient.setQueryData(["events", params.id], newEvent);
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", params.id]);
+    },
   });
 
   function handleSubmit(formData) {
     mutate({ id: params.id, event: formData });
-    navigate('../');
+    navigate("../");
   }
 
   function handleClose() {
